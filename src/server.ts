fa358fe3d62b5583171e0269f91f1412438dc20c
alfredo chivela <alfredochivela3@gmail.com>
2024-11-05 21:22:54 +0100

    updated all project

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express from 'express'
 
 const app = express();
+app.use(express.json());
 
-app.get("Hello",(req,res)=>{
+app.get("/hello",(req,res)=>{
     res.send();
+});
+
+app.post("/courses",(req,res)=>{
+    const {name} = req.body;
+
+    res.send(name);
 })
 
 app.listen(3000,()=>console.log("Server Running!"));
