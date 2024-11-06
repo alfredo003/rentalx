f27c0f4689070c37d8a7b51b9f8f4d9ec1c429c4
alfredo chivela <alfredochivela3@gmail.com>
2024-11-06 22:04:25 +0100

    add category repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,14 @@
 import { Router } from 'express';
 import { Category } from '../model/Category';
+import { CategoryRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-const categories:Category[] = [];
+const categoriesRespository = new CategoryRepository();
+
 categoriesRoutes.post("/",(req,res)=>{
     const {name,description} = req.body;
 
-    const category = new Category();
-    Object.assign(category,{
-        name,
-        description,
-        created_at: new Date()
-    });
-    categories.push(category);
+    categoriesRespository.create({name, description})
     return res.status(201).send();
 });
 export {categoriesRoutes}
