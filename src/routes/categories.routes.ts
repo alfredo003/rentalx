66d51985824a848721bb648e0a5755cb0abe23fe
alfredo chivela <alfredochivela3@gmail.com>
2024-11-08 07:51:34 +0100

    validate register category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,7 +8,15 @@ const categoriesRespository = new CategoryRepository();
 categoriesRoutes.post("/",(req,res)=>{
     const {name,description} = req.body;
 
+    const categoryAlreadyExists = categoriesRespository.findByName(name);
+    if(categoryAlreadyExists)
+        return res.status(400).json({error:"Category already exists!"});
     categoriesRespository.create({name, description})
     return res.status(201).send();
 });
+categoriesRoutes.get('/',(req,res)=>{
+    const all = categoriesRespository.list();
+    return res.json(all);
+});
+
 export {categoriesRoutes}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import express from 'express'
+import express from 'express';
 import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 app.use(express.json());
 
+app.use('/categories', categoriesRoutes);
 
-app.use("/categories",categoriesRoutes);
-
-app.listen(3000,()=>console.log("Server Running!"));
+app.listen(3000, () => console.log('Server Running!'));
