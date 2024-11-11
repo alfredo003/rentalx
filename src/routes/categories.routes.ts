bc0a666a738c8c3c077a895b34ce5a85ec0a3099
alfredo chivela <alfredochivela3@gmail.com>
2024-11-11 20:32:39 +0100

    add useCase Category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,9 @@
 import { Router } from 'express';
-import { CategoryRepository } from '../modules/cars/repositories/CategoriesRepository';
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
-
+import { createCategoryController } from '../modules/cars/useCase/createCategory';
 const categoriesRoutes = Router();
-const categoriesRespository = new CategoryRepository();
 
 categoriesRoutes.post('/', (req, res) => {
-  const { name, description } = req.body;
-
-  const createCategoryService = new CreateCategoryService(
-    categoriesRespository,
-  );
-
-  createCategoryService.execute({ name, description });
-
-  return res.status(201).send();
+  return createCategoryController.handle(req,res)
 });
 categoriesRoutes.get('/', (req, res) => {
   const all = categoriesRespository.list();
