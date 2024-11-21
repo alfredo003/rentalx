14033016e0a7a9a064e81b0229cc380a141a63bf
alfredo003 <alfredochivela3@gmail.com>
2024-11-21 12:56:14 +0100

    updated

--- src/modules/cars/useCase/importCategory/index.ts
+++ src/modules/cars/useCase/importCategory/index.ts
@@ -1,6 +1,6 @@
 import { CategoryRepository } from "../../repositories/implementations/CategoriesRepository";
 import { ImportCategoryController } from "./ImportCategoryController";
-import { ImportCategoryUseCase } from "./ImportCategoryUsecase";
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 const categoriesRepository = CategoryRepository.getInstance();
 const importeCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 const importCategoryController = new ImportCategoryController(importeCategoryUseCase);
