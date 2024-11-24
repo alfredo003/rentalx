193001577f2f618be1aa74690b3f8ad703b58463
alfredo003 <alfredochivela3@gmail.com>
2024-11-24 22:59:12 +0100

    updated

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { router } from './routes';
 import swaggerUI from "swagger-ui-express";
 import swaggerFile from "./swagger.json";
+
+import "./database"
 const app = express();
 
 app.use(express.json());
